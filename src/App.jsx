793e672ba94d41df9d7f7ebf788cbb41771cad92
juanb86 <juan.e.begnis@gmail.com>
2020-07-20 12:09:24 -0300

    React Router Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 import Users from "./components/users/Users";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -9,6 +12,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   searchUsers = async (text) => {
@@ -20,20 +24,44 @@ class App extends Component {
   };
 
   clearUsers = () => {
-    console.log("clear")
-    this.setState({users: [],loading: false})
+    console.log("clear");
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
   };
 
   render() {
-    const {users,loading} = this.state;
+    const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length>0 ? true : false} />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -10,14 +10,21 @@ class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    const { setAlert,searchUsers } = this.props;
+    if (this.state.text === "") {
+      setAlert("Please enter something", "Light");
+    } else {
+      e.preventDefault();
+      searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
@@ -25,25 +32,22 @@ class Search extends Component {
 
     return (
       <div>
-        <form onSubmit={this.onSubmit} className="form">
+        <form onSubmit={this.onSubmit} className='form'>
           <input
-            type="text"
-            name="text"
-            placeholder="Search users..."
+            type='text'
+            name='text'
+            placeholder='Search users...'
             value={this.state.text}
             onChange={this.onChange}
           />
           <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
           />
         </form>
         {showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={clearUsers}
-          >
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
             Clear
           </button>
         )}
